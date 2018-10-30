cee6abd7274cd6c023ac4240ddeaa55d9ea88130
Andreas Kutscheid <andreas@flauschhaus.org>
2018-10-31 00:49:28 +0100

    add some comments, lower and upper bound

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,20 @@ use rand::distributions::{Distribution, Uniform};
 use std::io;
 
 fn get_input () -> i64 {
-  let mut input = String::new();
   loop {
+    // Reading from stdin failed in general
+    let mut input = String::new();
     if let Err(e) = io::stdin().read_line(&mut input)
     {
       println!("Keine Zeichen gelesen. Fehler {0}", e);
       continue;
     }
+    // Conversion to integer succeeded
     if let Ok(n) = input.trim().parse::<i64>()
     {
       return n;
     }
+    // Conversion to integer failed
     else
     {
       println!("Keine Zahl gelesen. Nochmal");
@@ -25,11 +28,14 @@ fn get_input () -> i64 {
 
 fn main () {
   let mut rng = rand::thread_rng();
+  let low_bound = 2;
+  let up_bound = 10;
 
-  println!("Lass uns rechnen:");
+  println!("Lass uns rechnen in den Pyramiden von {0} bis {1}:", low_bound, up_bound);
+  println!("-------------------------------------------------");
 
   loop {
-    let pyramid_range = Uniform::new_inclusive(2, 10);
+    let pyramid_range = Uniform::new_inclusive(low_bound, up_bound);
     let pyramid = pyramid_range.sample(&mut rng);
     let sample_range = Uniform::new_inclusive(0,pyramid);
     let sample = sample_range.sample(&mut rng);
